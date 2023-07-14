0a33fbd978d985f3c884f4983c05950383bffb0e
Katie Toney <kathryn.toney@gmail.com>
2023-07-14 11:43:02 -0400

    more updates

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 
 
 
-const PORT =  3001
+const PORT = 3001
+app.use(cors({ origin: "https://pocketsomm.netlify.app" }))
 
 app.use(require('./routes/authentication.js'))
 
 
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
